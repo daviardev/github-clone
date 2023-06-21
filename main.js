8a2e093ccd8d5e1fce0df0e7aaf28c971ea8f244
daviardev <jerson.silva99@hotmail.com>
2023-06-21 11:41:32 -0500

    Change color header

--- main.js
+++ main.js
@@ -1,2 +1,35 @@
 import './style.css'
 import './js/theme'
+
+import { $ } from './src/lib/dom'
+
+/**
+ *
+ * @param {NodeList} e
+ * @param {String} event
+ * @param {Function} callback
+ * @param {NodeList} header
+ */
+
+const addEOnElement = (e, event, callback) => {
+  for (const i of e) {
+    i.addEventListener(event, callback)
+  }
+}
+
+const header = $('[data-header]')
+const searchField = $('[data-search-field]')
+const searchToggler = $('[data-search-toggler]')
+
+let isExpanded = false
+
+window.addEventListener('scroll', () => {
+  header.classList[window.scrollY > 50 ? 'add' : 'remove']('active')
+})
+
+searchToggler.addEventListener('click', () => {
+  header.classList.toggle('search-active')
+  isExpanded = !isExpanded
+  this.setAttribute('aria-expanded', isExpanded)
+  searchField.focus()
+})
